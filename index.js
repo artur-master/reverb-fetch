45c9939c55386e6c25d26cbef1c4a84ae1b368f2
Fullstack-Dev <arturshayakhmetov4@gmail.com>
2021-08-31 08:06:43 -0400

    eBay fetch price

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 Axios=require('axios');
 
-const apiUrl = "https://api.reverb.com/api";
-
-async function getPricingInfo(brand_name, model) {
-  const response = await Axios.get(`${apiUrl}/csps/${brand_name}-${model}/transactions?per_page=10`,
+async function getPricingInfo(listItem) {
+  const response = await Axios.get(`https://www.ebay.com/b/${listItem}?rt=nc&mag=1&LH_BIN=1&LH_ItemCondition=3000&LH_PrefLoc=3`,
     {
       headers: {
-        "accept-language": "en",
-        "accept-version": "3.0"
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9"
       }
-    }
-  )
-  const allowTypes = ['Mint','Excellent','Very Good','Good','Fair','B-Stock'];
-  const result = [];
-
-  if(response.data) {
-    response.data.transactions.forEach(transaction => {
-      if(allowTypes.includes(transaction["condition"]))
-        result.push(transaction["price_ask"]["amount"]);
     });
-  }
 
-  return result;
+  const regex = /<li class=\"s-item s-item--large s-item--bgcolored\">(.*?)<\/li>/gi;
+  const arr = response.data.match(regex);
+
+  const titleEx = /<h3 class="s-item__title*[^>]+?">(.*?)<\/h3>/g;
+  const priceEx = /<span class="s-item__price">\$(.*?)<\/span>/g;
+  const shippingEx = /<span class="s-item__shipping s-item__logisticsCost">\$(.*)?( shipping)<\/span>/g;
+
+  const re = arr.map(element => {
+    const title = Array.from(element.matchAll(titleEx), m => m[1]);
+    const price = Array.from(element.matchAll(priceEx), m => m[1]);
+    const shipping = Array.from(element.matchAll(shippingEx), m => m[1]);
+
+    return {
+      title: title[0],
+      price: price[0],
+      shpping: shipping[0] || "Free"
+    }
+  });
+
+  return re;
 }
 
-getPricingInfo("dwarfcraft-devices", "wizard-of-pitch").then(re => {
+getPricingInfo("Guitar-Bass-Pedals/41411").then(re => {
   console.log(re);
 });
